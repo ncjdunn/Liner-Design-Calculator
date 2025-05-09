eba5b361e122b58d9c3a81e00785bfe1208b0ef5
ncjdunn <ncjdunn@gmail.com>
2025-05-09 12:46:46 -0400

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,17 @@
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open('liner-design-cache').then((cache) => {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/manifest.json',
+        '/service-worker.js'
+      ]);
+    })
+  );
+});
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => response || fetch(e.request))
+  );
+});
